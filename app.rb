4157adc12ec4c593122c10e4983ad069e6f1ab5e
Ariane <xu.ariane@hotmail.fr>
2020-03-03 16:24:21 +0000

    entering players

--- app.rb
+++ app.rb
@@ -1,12 +1,30 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Hello Battle!'
-    'Testing infrastructure working!'
+  # get '/' do
+  #   'Hello Battle!'
+  #   'Testing infrastructure working!'
+  # end
 
+  get '/' do
     erb :index
   end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+
+
+
+
+
+
+
+
+
 
 
  # start the server if ruby file executed directly
